c185aa4c1fbb9c6945ddd4de83e0dc6d75da445b
pranayhbwsl <pranay.chahare@hbwsl.com>
2022-02-21 12:58:00 +0530

    TExt

--- pwa-installer.sh
+++ pwa-installer.sh
@@ -50,6 +50,6 @@ yarn add file:@hbwsl/deals
 yarn add sass sass-loader@^10.1.1 file-saver axios
 
 WEBPACK_CONFIG_EXT="config.module.rules.push({test: /\.s[ca]ss$/,use: ['style-loader',{loader: require.resolve('css-loader'),options: {importLoaders: 1,modules: true,modules : {localIdentName: '[name]__[local]__[hash:base64:3]',},},},'sass-loader']});"
-&& sed  -i".bak" -e  "/return \[config\]/i $WEBPACK_CONFIG_EXT" /workspace/pwa-deals/webpack.config.js
+sed  -i".bak" -e  "/return \[config\]/i $WEBPACK_CONFIG_EXT" /workspace/pwa-deals/webpack.config.js
 yarn run build && yarn run start
 fi
